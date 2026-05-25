5a8a39fe6dc07d0099aa4bdf0521bd5054709768
Mehrdad <MehSohayl@gmail.com>
2026-05-25 23:15:16 +0330

    feat(Identity.Domain): simple Person aggregate
    Added a minimal identity aggregate with creation helper.

---
+++ src/Modules/Identity/Identity.Domain/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using BuildingBlocks.Domain.Entities;
+
+namespace Identity.Domain.Entities;
+
+public class Person : Entity
+{
+    public string Name { get; private set; }
+    public DateTime RegisteredAt { get; private set; }
+
+    private Person() { }
+
+    private Person(string name)
+    {
+        Name = name;
+        RegisteredAt = DateTime.UtcNow;
+    }
+
+    public static Person Create(string name)
+        => new Person(name);
+
+}
