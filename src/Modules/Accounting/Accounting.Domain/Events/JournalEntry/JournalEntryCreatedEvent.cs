07c6a850b7c13e798beddaaa8caddff6648771e1
Mehrdad <MehSohayl@gmail.com>
2026-05-27 15:56:48 +0330

    feat(JournalEntry): add JournalEntryCreatedEvent domain event
    Introduced a new record type to represent the creation of a journal entry,
    including description, timestamp, correlation id, and status.

---
+++ src/Modules/Accounting/Accounting.Domain/Events/JournalEntry/JournalEntryCreatedEvent.cs
@@ -0,0 +1,9 @@
+﻿using Accounting.Domain.Enums;
+using BuildingBlocks.Domain.Events;
+
+namespace Accounting.Domain.Events.JournalEntry;
+
+public record JournalEntryCreatedEvent(string description,
+    DateTime createdAt,
+    Guid correlationId,
+    JournalEntryStatus status) : DomainEventBase;
